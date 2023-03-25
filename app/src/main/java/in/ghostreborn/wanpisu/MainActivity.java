fa29926d62f15dacf5552180d0cba20e89466cac
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-03-25 15:51:10 +0530

    Custom Adapter

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -1,8 +1,15 @@
 package in.ghostreborn.wanpisu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import in.ghostreborn.wanpisu.adapter.AnimeSearchAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView animeContainerView = findViewById(R.id.anime_container);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        animeContainerView.setLayoutManager(gridLayoutManager);
+        ArrayList<String> animes = new ArrayList<>();
+        animes.add("One Piece");
+        animes.add("Bleach");
+        animes.add("Naruto");
+        animes.add("Dragon Ball Z");
+        animes.add("Demon Slayer");
+        AnimeSearchAdapter adapter = new AnimeSearchAdapter(animes);
+        animeContainerView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/in/ghostreborn/wanpisu/adapter/AnimeSearchAdapter.java
@@ -0,0 +1,54 @@
+package in.ghostreborn.wanpisu.adapter;
+
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import in.ghostreborn.wanpisu.R;
+
+public class AnimeSearchAdapter extends RecyclerView.Adapter<AnimeSearchAdapter.ViewHolder> {
+
+    private final ArrayList<String> animeNames;
+
+    public AnimeSearchAdapter(ArrayList<String> mAnimeNames){
+        animeNames = mAnimeNames;
+    }
+
+    @NonNull
+    @Override
+    public AnimeSearchAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.anime_search_list_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnimeSearchAdapter.ViewHolder holder, int position) {
+        holder.animeTextView.setText(animeNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return animeNames.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView animeTextView;
+        public ImageView animeImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            animeTextView = itemView.findViewById(R.id.anime_text_view);
+            animeImageView = itemView.findViewById(R.id.anime_image_view);
+        }
+    }
+}
