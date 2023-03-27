932b964e985d61885d092cfeea1f4028016436b0
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-03-28 02:42:07 +0530

    Add AnilistActivity * Button click move to AnilistActivity

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import in.ghostreborn.wanpisu.ui.AnilistActivity;
 import in.ghostreborn.wanpisu.ui.WanPisuActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView anilistTextView = findViewById(R.id.anilist_text_view);
+        anilistTextView.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, AnilistActivity.class));
+        });
 
         TextView watchAnimeTextView = findViewById(R.id.watch_anime_text_view);
         watchAnimeTextView.setOnClickListener(view -> {
---
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/AnilistActivity.java
@@ -0,0 +1,16 @@
+package in.ghostreborn.wanpisu.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.ghostreborn.wanpisu.R;
+
+public class AnilistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anilist);
+    }
+}
