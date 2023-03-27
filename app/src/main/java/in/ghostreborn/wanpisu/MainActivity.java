2c5b4cc45308ebcbbbec9862c09be64a19d918ab
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-03-28 02:35:11 +0530

    Update UI

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -1,19 +1,8 @@
 package in.ghostreborn.wanpisu;
 
 import android.os.Bundle;
-import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
-import in.ghostreborn.wanpisu.adapter.AnimeSearchAdapter;
-import in.ghostreborn.wanpisu.model.WanPisu;
-import in.ghostreborn.wanpisu.parser.AllAnime;
 
 public class MainActivity extends AppCompatActivity {
 
