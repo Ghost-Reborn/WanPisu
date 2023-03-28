01afe58418661932aa388d6fa398b6afa4572dbe
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-03-28 20:24:28 +0530

    Store API Token as SharedPreference

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -1,11 +1,16 @@
 package in.ghostreborn.wanpisu;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import in.ghostreborn.wanpisu.constants.WanPisuConstants;
 import in.ghostreborn.wanpisu.ui.AnilistLoginActivity;
 import in.ghostreborn.wanpisu.ui.WanPisuActivity;
 
@@ -15,6 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = getSharedPreferences(WanPisuConstants.WAN_PISU_PREFERENCE, Context.MODE_PRIVATE);
+        Intent urlIntent = getIntent();
+        Uri data = urlIntent.getData();
+        if (data != null){
+            String url = data.toString();
+            String token = url.substring(
+                    url.indexOf("token=") + 6,
+                    url.indexOf("&token_type")
+            );
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(WanPisuConstants.WAN_PISU_ANILIST_TOKEN, token);
+            editor.apply();
+        }
 
         TextView anilistTextView = findViewById(R.id.anilist_text_view);
         anilistTextView.setOnClickListener(view -> {
--- app/src/main/java/in/ghostreborn/wanpisu/ui/AnilistLoginActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/AnilistLoginActivity.java
@@ -24,25 +24,16 @@ public class AnilistLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_anilist_login);
 
         SharedPreferences preferences = getSharedPreferences(WanPisuConstants.WAN_PISU_PREFERENCE, Context.MODE_PRIVATE);
-        if (preferences.contains(WanPisuConstants.WAN_PISU_ANILIST_TOKEN)){
-            Toast.makeText(this,
+        if (!preferences.contains(WanPisuConstants.WAN_PISU_ANILIST_TOKEN)){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Anilist.ANILIST_TOKEN_URL));
+            startActivity(intent);
+        }else {
+            Toast.makeText(
+                    this,
                     preferences.getString(WanPisuConstants.WAN_PISU_ANILIST_TOKEN, ""),
                     Toast.LENGTH_SHORT
-            ).show();
+                    ).show();
         }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Anilist.ANILIST_TOKEN_URL));
-        startActivity(intent);
-
-        EditText anilistTokenEditText = findViewById(R.id.anilist_token_edit_text);
-        Button anilistTokenSubmitButton = findViewById(R.id.anilist_token_submit_button);
-        anilistTokenSubmitButton.setOnClickListener(view -> {
-            if (!anilistTokenEditText.getText().toString().equals("")){
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString(WanPisuConstants.WAN_PISU_ANILIST_TOKEN, anilistTokenEditText.getText().toString());
-                editor.apply();
-            }
-        });
 
     }
 }
