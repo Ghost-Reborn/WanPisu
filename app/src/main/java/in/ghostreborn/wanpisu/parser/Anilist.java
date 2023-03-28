c1315142d8a7d4a85d5023f115307875fbc786eb
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-03-28 23:01:09 +0530

    Anilist API get username

--- app/src/main/java/in/ghostreborn/wanpisu/parser/Anilist.java
+++ app/src/main/java/in/ghostreborn/wanpisu/parser/Anilist.java
@@ -1,12 +1,69 @@
 package in.ghostreborn.wanpisu.parser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class Anilist {
 
+    public static final String LOG_TAG = "WAN_PISU";
+
+    // Token URLs
     public static final String CLIENT_ID = "11820";
     public static final String ANILIST_BASE_URL = "https://anilist.co/";
-    public static final String ANILIST_TOKEN_URL = ANILIST_BASE_URL +
-            "api/v2/oauth/authorize?client_id=" +
-            CLIENT_ID +
-            "&response_type=token";
+    public static final String ANILIST_TOKEN_URL = ANILIST_BASE_URL + "api/v2/oauth/authorize?client_id=" + CLIENT_ID + "&response_type=token";
+
+    // Query API URLs
+    public static final String QUERY_API_BASE = "https://graphql.anilist.co/";
+
+    public static String getAnilistUserName(String ACCESS_TOKEN) {
+        String QUERY = "query { Viewer { name } }";
+        String AUTHORIZATION_HEADER = "Authorization";
+        String TOKEN_PREFIX = "Bearer ";
+
+        try{
+            URL url = new URL(QUERY_API_BASE);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty(AUTHORIZATION_HEADER, TOKEN_PREFIX + ACCESS_TOKEN);
+
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("query", QUERY);
+            requestBody.put("variables", new JSONObject());
+
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(requestBody.toString().getBytes());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            conn.disconnect();
+
+            String user = new JSONObject(response.toString())
+                    .getJSONObject("data")
+                    .getJSONObject("Viewer")
+                    .getString("name");
+
+            return user;
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return "CHECK INTERNET CONNECTION AND TRY AGAIN";
+
+    }
 
 }
--- app/src/main/java/in/ghostreborn/wanpisu/ui/AnilistActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/AnilistActivity.java
@@ -6,39 +6,24 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 
 import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
 import in.ghostreborn.wanpisu.parser.Anilist;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class AnilistActivity extends AppCompatActivity {
+
+    static TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anilist);
+
+        testText = findViewById(R.id.test_text);
 
         SharedPreferences preferences = getSharedPreferences(WanPisuConstants.WAN_PISU_PREFERENCE, Context.MODE_PRIVATE);
         if (!preferences.contains(WanPisuConstants.WAN_PISU_ANILIST_TOKEN)){
@@ -52,76 +37,23 @@ public class AnilistActivity extends AppCompatActivity {
 
     }
 
-    static class AnilistAsync extends AsyncTask<Void, Void, Void>{
+    static class AnilistAsync extends AsyncTask<Void, Void, String> {
 
-        private static final String TAG = "WanPisu";
-        private static final String BASE_URL = "https://graphql.anilist.co";
-        private static String ACCESS_TOKEN;
+        String TOKEN;
 
-        public AnilistAsync(String token){
-            ACCESS_TOKEN = token;
+        public AnilistAsync(String TOKEN){
+            this.TOKEN = TOKEN;
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            try{
-                OkHttpClient client = new OkHttpClient();
-                HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL).newBuilder();
-                String url = urlBuilder.build().toString();
+        protected String doInBackground(Void... voids) {
+            return Anilist.getAnilistUserName(TOKEN);
+        }
 
-                JSONObject variables = new JSONObject();
-                variables.put("status", "RELEASING");
-                variables.put("sort", "POPULARITY_DESC");
-                variables.put("perPage", 10);
-
-                JSONObject query = new JSONObject();
-                query.put("query", "query ($status: MediaStatus, $sort: [MediaSort], $perPage: Int) {\n" +
-                        "  Page(page: 1, perPage: $perPage) {\n" +
-                        "    media(status: $status, sort: $sort) {\n" +
-                        "      id\n" +
-                        "      title {\n" +
-                        "        romaji\n" +
-                        "        english\n" +
-                        "        native\n" +
-                        "      }\n" +
-                        "      episodes\n" +
-                        "      startDate {\n" +
-                        "        year\n" +
-                        "        month\n" +
-                        "        day\n" +
-                        "      }\n" +
-                        "      endDate {\n" +
-                        "        year\n" +
-                        "        month\n" +
-                        "        day\n" +
-                        "      }\n" +
-                        "      status\n" +
-                        "      averageScore\n" +
-                        "      genres\n" +
-                        "      description\n" +
-                        "    }\n" +
-                        "  }\n" +
-                        "}");
-                query.put("variables", variables);
-
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(okhttp3.RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), query.toString()))
-                        .addHeader("Authorization", "Bearer " + ACCESS_TOKEN)
-                        .build();
-
-                Response response = client.newCall(request).execute();
-                StringBuilder out = new StringBuilder();
-                out.append(response.body().string());
-
-                Log.e(TAG, out.toString());
-
-                return null;
-
-            } catch (JSONException | IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            testText.setText(s);
         }
     }
 
