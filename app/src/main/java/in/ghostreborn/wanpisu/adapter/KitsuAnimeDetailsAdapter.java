5dcad6338b496ca0ed39571fe2fcd97a40db13fc
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-02 23:49:39 +0530

    Introduce watch button

--- app/src/main/java/in/ghostreborn/wanpisu/adapter/KitsuAnimeDetailsAdapter.java
+++ app/src/main/java/in/ghostreborn/wanpisu/adapter/KitsuAnimeDetailsAdapter.java
@@ -1,8 +1,11 @@
 package in.ghostreborn.wanpisu.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +16,7 @@ import com.squareup.picasso.Picasso;
 
 import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
+import in.ghostreborn.wanpisu.ui.WanPisuActivity;
 
 public class KitsuAnimeDetailsAdapter extends RecyclerView.Adapter<KitsuAnimeDetailsAdapter.ViewHolder>{
     @NonNull
@@ -28,6 +32,12 @@ public class KitsuAnimeDetailsAdapter extends RecyclerView.Adapter<KitsuAnimeDet
         holder.kitsuDetailDescriptionView.setText(WanPisuConstants.kitsuDetails.get(position).getDescription());
         holder.kitsuDetailEpisodesView.setText(WanPisuConstants.kitsuDetails.get(position).getEpisodes());
         holder.kitsuDetailRatingView.setText(WanPisuConstants.kitsuDetails.get(position).getAverageRating());
+        holder.kitsuDetailWatchButton.setOnClickListener(view -> {
+            Context context = holder.itemView.getContext();
+            Intent watchIntent = new Intent(context, WanPisuActivity.class);
+            watchIntent.putExtra("ANIME_NAME", holder.kitsuDetailTextView.getText().toString());
+            context.startActivity(watchIntent);
+        });
         Picasso.get().load(WanPisuConstants.kitsuDetails.get(position).getThumbnail()).into(holder.kitsuDetailImageView);
     }
 
@@ -42,6 +52,7 @@ public class KitsuAnimeDetailsAdapter extends RecyclerView.Adapter<KitsuAnimeDet
         public TextView kitsuDetailDescriptionView;
         public TextView kitsuDetailEpisodesView;
         public TextView kitsuDetailRatingView;
+        public Button kitsuDetailWatchButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -50,6 +61,7 @@ public class KitsuAnimeDetailsAdapter extends RecyclerView.Adapter<KitsuAnimeDet
             kitsuDetailDescriptionView = itemView.findViewById(R.id.kitsu_detail_description_view);
             kitsuDetailEpisodesView = itemView.findViewById(R.id.kitsu_detail_episodes_view);
             kitsuDetailRatingView = itemView.findViewById(R.id.kitsu_detail_rating_view);
+            kitsuDetailWatchButton = itemView.findViewById(R.id.kitsu_detail_watch_button);
         }
     }
 }
