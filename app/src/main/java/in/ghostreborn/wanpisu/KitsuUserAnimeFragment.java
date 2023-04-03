26ed6ca4509a48dbbab609ba6a3c1f6c6fdc4e76
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-04 03:05:45 +0530

    Tapping the textview takes to login screen

--- app/src/main/java/in/ghostreborn/wanpisu/KitsuUserAnimeFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/KitsuUserAnimeFragment.java
@@ -1,5 +1,6 @@
 package in.ghostreborn.wanpisu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
+import in.ghostreborn.wanpisu.ui.KitsuLoginActivity;
 
 public class KitsuUserAnimeFragment extends Fragment {
 
@@ -20,6 +22,11 @@ public class KitsuUserAnimeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_kitsu_user_anime, container, false);
         RecyclerView kitsuUserAnimeRecycler = view.findViewById(R.id.kitsu_user_anime_recycler);
         TextView kitsuLoginInfoButton = view.findViewById(R.id.kitsu_login_info_button);
+        kitsuLoginInfoButton.setOnClickListener(view1 -> {
+            if (!WanPisuConstants.preferences.contains(WanPisuConstants.KITSU_LOGIN_FINISHED)) {
+                startActivity(new Intent(getContext(), KitsuLoginActivity.class));
+            }
+        });
         if (!WanPisuConstants.preferences.contains(WanPisuConstants.KITSU_LOGIN_FINISHED)) {
             kitsuLoginInfoButton.setVisibility(View.VISIBLE);
         }
