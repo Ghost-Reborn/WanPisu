30f09ced350c15d34b982264e94343fe862f51ee
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-05 21:14:08 +0530

    Bulk Download: Custom Download Manager

--- app/src/main/java/in/ghostreborn/wanpisu/AnimeDownloaderActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/AnimeDownloaderActivity.java
@@ -1,17 +1,15 @@
 package in.ghostreborn.wanpisu;
 
-import android.app.DownloadManager;
-import android.content.Context;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
@@ -44,24 +42,29 @@ public class AnimeDownloaderActivity extends AppCompatActivity {
         protected String doInBackground(Void... voids) {
             String episodes = animeEpisodeEditText.getText().toString();
             ArrayList<String> servers = AllAnime.getAnimeServer(WanPisuConstants.preferences.getString(WanPisuConstants.ALL_ANIME_ANIME_ID, ""), episodes);
+            String srvr = "";
             for (String server: servers) {
                 if (server.contains("workfields")){
-                    return server;
+                    srvr = server;
                 }
+            }
+            WanPisuDownloadManager downloadManager = new WanPisuDownloadManager();
+            try {
+                downloadManager.download(srvr, "/sdcard/file.mp4",new WanPisuDownloadManager.ProgressListener() {
+                    @Override
+                    public void onProgress(long bytesRead, long contentLength, boolean done) {
+                        Log.e("DOWNLOADING: ", bytesRead + " / " + contentLength);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             return "";
         }
 
         @Override
         protected void onPostExecute(String server) {
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(server));
-            request.setTitle("Video Download"); // Set the title of the download
-            request.setDescription("Downloading video"); // Set the description of the download
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); // Show a notification when the download is complete
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "video.mp4"); // Set the destination directory and filename
 
-            DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            long downloadId = downloadManager.enqueue(request);
         }
     }
 
---
+++ app/src/main/java/in/ghostreborn/wanpisu/WanPisuDownloadManager.java
@@ -0,0 +1,58 @@
+package in.ghostreborn.wanpisu;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class WanPisuDownloadManager {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public interface ProgressListener {
+        void onProgress(long bytesRead, long contentLength, boolean done);
+    }
+
+    public void download(String url, String destPath, ProgressListener listener) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Failed to download file: " + response);
+        }
+
+        ResponseBody body = response.body();
+        long contentLength = body.contentLength();
+
+        InputStream inputStream = body.byteStream();
+
+        File destFile = new File(destPath);
+        FileOutputStream outputStream = new FileOutputStream(destFile);
+
+        byte[] buffer = new byte[4096];
+        int len;
+        long downloaded = 0;
+
+        while ((len = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
+            downloaded += len;
+            listener.onProgress(downloaded, contentLength, false);
+        }
+
+        outputStream.flush();
+
+        if (downloaded != contentLength) {
+            throw new IOException("Failed to download file: downloaded " + downloaded + " bytes, expected " + contentLength);
+        }
+
+        listener.onProgress(downloaded, contentLength, true);
+    }
+
+}
