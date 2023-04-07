75430d82754e48558a56bf9c45f9d8598e0cb5e9
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-08 00:33:08 +0530

    Show more episode selection in episode selection * It does nothing

---
+++ app/src/main/java/in/ghostreborn/wanpisu/adapter/AnimeCategorizeAdapter.java
@@ -0,0 +1,56 @@
+package in.ghostreborn.wanpisu.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import in.ghostreborn.wanpisu.R;
+
+public class AnimeCategorizeAdapter extends RecyclerView.Adapter<AnimeCategorizeAdapter.ViewHolder>{
+
+    int startEpisode = 1;
+    int endEpisode, total;
+    public AnimeCategorizeAdapter(String endEpisode){
+        this.endEpisode = Integer.parseInt(endEpisode);
+        this.total = this.endEpisode / 20;
+    }
+
+
+    @NonNull
+    @Override
+    public AnimeCategorizeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.episode_categorize_list, parent, false);
+        return new AnimeCategorizeAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnimeCategorizeAdapter.ViewHolder holder, int position) {
+        holder.startEpisode.setText(String.valueOf(startEpisode));
+        holder.endEpisode.setText(String.valueOf(startEpisode + 19));
+        startEpisode = startEpisode + 20;
+    }
+
+    @Override
+    public int getItemCount() {
+        return total;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView startEpisode;
+        TextView endEpisode;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            startEpisode = itemView.findViewById(R.id.anime_categorize_start_text_view);
+            endEpisode = itemView.findViewById(R.id.anime_categorize_end_text_view);
+
+        }
+    }
+}
--- app/src/main/java/in/ghostreborn/wanpisu/ui/EpisodesSelectActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/EpisodesSelectActivity.java
@@ -3,16 +3,17 @@ package in.ghostreborn.wanpisu.ui;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 import in.ghostreborn.wanpisu.R;
+import in.ghostreborn.wanpisu.adapter.AnimeCategorizeAdapter;
 import in.ghostreborn.wanpisu.adapter.AnimeEpisodesAdapter;
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
 import in.ghostreborn.wanpisu.model.Kitsu;
@@ -20,14 +21,16 @@ import in.ghostreborn.wanpisu.parser.KitsuAPI;
 
 public class EpisodesSelectActivity extends AppCompatActivity {
 
-    RecyclerView animeContainerView;
+    RecyclerView animeEpisodesRecycler;
+    RecyclerView animeCategorizeRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episodes_select);
 
-        animeContainerView = findViewById(R.id.anime_episode_recycler_view);
+        animeEpisodesRecycler = findViewById(R.id.anime_episode_recycler_view);
+        animeCategorizeRecycler = findViewById(R.id.episode_categorize_recycler_view);
         new KitsuEpisodeTask().execute();
         ImageView animeDownloadView = findViewById(R.id.anime_download_view);
         animeDownloadView.setOnClickListener(view -> {
@@ -47,13 +50,19 @@ public class EpisodesSelectActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(ArrayList<Kitsu> kitsus) {
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-            animeContainerView.setLayoutManager(linearLayoutManager);
+            animeEpisodesRecycler.setLayoutManager(linearLayoutManager);
             AnimeEpisodesAdapter adapter = new AnimeEpisodesAdapter(
                     EpisodesSelectActivity.this,
                     WanPisuConstants.preferences
                             .getString(WanPisuConstants.ALL_ANIME_ANIME_ID, "")
             );
-            animeContainerView.setAdapter(adapter);
+            animeEpisodesRecycler.setAdapter(adapter);
+            AnimeCategorizeAdapter animeCategorizeAdapter = new AnimeCategorizeAdapter(
+                    WanPisuConstants.preferences.getString(WanPisuConstants.ALL_ANIME_ANIME_EPISODES, "1")
+            );
+            GridLayoutManager manager = new GridLayoutManager(getBaseContext(), 1, GridLayoutManager.HORIZONTAL, false);
+            animeCategorizeRecycler.setLayoutManager(manager);
+            animeCategorizeRecycler.setAdapter(animeCategorizeAdapter);
         }
     }
 
