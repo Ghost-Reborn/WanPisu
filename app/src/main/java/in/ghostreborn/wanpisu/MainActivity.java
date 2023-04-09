65c3454c65115ae89aed3d649280dec182f013b2
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-09 15:35:42 +0530

    Show AllAnime in MainActivity

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -5,15 +5,29 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import in.ghostreborn.wanpisu.adapter.AnimeSearchAdapter;
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
+import in.ghostreborn.wanpisu.model.WanPisu;
+import in.ghostreborn.wanpisu.parser.AllAnime;
 import in.ghostreborn.wanpisu.ui.AnilistActivity;
 import in.ghostreborn.wanpisu.ui.WanPisuActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<WanPisu> animeDetailsArray;
+    AnimeSearchAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +36,38 @@ public class MainActivity extends AppCompatActivity {
 
         getAnilistTokenFromIntentFilter();
 
-        TextView anilistTextView = findViewById(R.id.anilist_text_view);
-        anilistTextView.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, AnilistActivity.class));
-        });
+        RecyclerView animeContainerView = findViewById(R.id.anime_container);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        animeContainerView.setLayoutManager(linearLayoutManager);
 
-        TextView watchAnimeTextView = findViewById(R.id.watch_anime_text_view);
-        watchAnimeTextView.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, WanPisuActivity.class));
+        // Get latest anime updates
+        Executor executor = Executors.newSingleThreadExecutor();
+        Runnable task = () -> {
+            animeDetailsArray = AllAnime.parseAnimeIDAnimeNameAnimeThumbnail("");
+            adapter = new AnimeSearchAdapter(MainActivity.this,animeDetailsArray);
+            runOnUiThread(() -> animeContainerView.setAdapter(adapter));
+        };
+        executor.execute(task);
+
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Runnable task = () -> {
+                    animeDetailsArray = AllAnime.parseAnimeIDAnimeNameAnimeThumbnail(
+                            searchView.getQuery().toString()
+                    );
+                    adapter = new AnimeSearchAdapter(MainActivity.this, animeDetailsArray);
+                    runOnUiThread(() -> animeContainerView.setAdapter(adapter));
+                };
+                executor.execute(task);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
         });
 
     }
