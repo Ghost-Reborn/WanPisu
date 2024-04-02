e1d6703ebe7ca912e83f076491b97cca56d3f880
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-04-03 01:59:22 +0530

    Update UI

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -1,8 +1,14 @@
 package in.ghostreborn.wanpisu;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView allAnimeContainer = findViewById(R.id.all_anime_container);
+        SearchView animeSearchView = findViewById(R.id.anime_search_view);
+
+        // Default selection is Home
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.findItem(R.id.menu_home);
+        menuItem.setChecked(true);
+
     }
 
 }
