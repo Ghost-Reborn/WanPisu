6781a396542a85a64d9b2a950282e8dcaddab800
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-04-05 01:29:41 +0530

    Drop more unwanted stuffs

--- app/src/main/java/in/ghostreborn/wanpisu/ui/AnimeDetailsActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/AnimeDetailsActivity.java
@@ -1,37 +1,17 @@
-package in.ghostreborn.wanpisu;
+package in.ghostreborn.wanpisu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
+import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.WanPisuConstants;
-import in.ghostreborn.wanpisu.parser.JikanParser;
-import in.ghostreborn.wanpisu.ui.AnimeEpisodesActivity;
-import in.ghostreborn.wanpisu.utils.AnilistUtils;
-import in.ghostreborn.wanpisu.utils.GraphQlRequestBody;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class AnimeDetailsActivity extends AppCompatActivity {
 
