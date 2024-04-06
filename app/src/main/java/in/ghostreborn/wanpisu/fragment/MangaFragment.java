b8bfd9a11680e12e3f52d0a86890be021a9e538d
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-04-06 08:53:42 +0530

    Refactor

--- app/src/main/java/in/ghostreborn/wanpisu/fragment/MangaFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/MangaFragment.java
@@ -10,27 +10,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import in.ghostreborn.wanpisu.R;
-import in.ghostreborn.wanpisu.adapter.AnimeSearchAdapter;
 import in.ghostreborn.wanpisu.adapter.MangaSearchAdapter;
-import in.ghostreborn.wanpisu.model.AllManga;
-import in.ghostreborn.wanpisu.model.Manga;
-import in.ghostreborn.wanpisu.model.WanPisu;
-import in.ghostreborn.wanpisu.parser.AllAnime;
 import in.ghostreborn.wanpisu.parser.MangaParser;
 
-public class TestFragment extends Fragment {
+public class MangaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_manga, container, false);
         RecyclerView mangaContainerView = view.findViewById(R.id.manga_container);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(view.getContext(), 3);
         mangaContainerView.setLayoutManager(gridLayoutManager);
